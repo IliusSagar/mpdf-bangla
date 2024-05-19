e00a1388c6946dca51266682bc97d43f5484a766
IliusSagar <sagar01830@gmail.com>
2024-05-19 13:35:10 +0600

    mdf install & bangla font ttf download

---
+++ resources/views/Note.blade.php
@@ -0,0 +1,4 @@
+** composer create-project laravel/laravel example-app
+** composer require mpdf/mpdf
+** google search bangla font (www.omicronlab.com) - Download SolaimanLipi
+** public/fonts/SolaimanLipi_20-04-07.ttf
